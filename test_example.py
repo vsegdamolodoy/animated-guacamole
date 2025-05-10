2c2d44e68f3e5c6ac0d4d2031778ecaa81273d43
vsegdamolodoy <mr.adamson005@gmail.com>
2025-05-10 19:23:57 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,24 +1,24 @@
 from playwright.sync_api import sync_playwright
 
-
 def test_example_page():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-
+        
         page.goto("https://example.com ")
-
-        assert "Example Domain" in page.title(), "Заголовок страницы не содержит 'Example'"
-        print("Проверка заголовка успешна.")
-
+        
+        assert "Example Domain" in page.title(), f"Заголовок неожиданный: {page.title()}"
+        print("Проверка заголовка прошла успешно.")
+        
         more_info_button = page.locator("a:has-text('More information')")
         more_info_button.click()
+        
+        page.wait_for_url("https://www.iana.org/help/example-domains ")
 
-        assert page.url == "https://www.iana.org/help/example-domains ", "Перенаправление не произошло на ожидаемую страницу"
-        print("Перенаправление успешно.")
-
+        assert page.url == "https://www.iana.org/help/example-domains ", f"Переход на неожиданный URL: {page.url}"
+        print("Перенаправление на страницу прошло успешно.")
+        
         browser.close()
-
 
 if __name__ == "__main__":
     test_example_page()
